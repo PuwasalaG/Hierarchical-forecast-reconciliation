46fd4b5dde2e688faac6d13ab71b419266488e1a
Unknown <george.athanasopoulos@monash.edu>
2020-01-28 19:56:48 +1100

    Expenditure Weights

--- Application-R/Application-TourismData/OrthVsOblq_projections/Original-scale_betterModels/makeweights.R
+++ Application-R/Application-TourismData/OrthVsOblq_projections/Original-scale_betterModels/makeweights.R
@@ -7,11 +7,24 @@ rm(list=ls())
 OvernightTrips_Region <- read_csv("OvernightTrips_2017.csv")[,-(1:3)]
 
 ## Replacing the odd value in `Adelaide Hills` with the average of its adjecent numbers ##
-Hierarchy <- suppressMessages(hts(OvernightTrips_Region, list(7, c(6,5,4,4,3,3,2), 
+HierarchyTrips <- suppressMessages(hts(OvernightTrips_Region, list(7, c(6,5,4,4,3,3,2), 
                                                                   c(2,2,1,4,4,1,3,1,3,6,7,3,4,3,2,3,
                                                                     3,4,2,3,1,1,1,2,2,3,4))))
-S <- smatrix(Hierarchy)
+S <- smatrix(HierarchyTrips)
 
-weights<-tibble(Series=unlist(Hierarchy$labels),Weight=rowSums(S))
+weights<-tibble(Series=unlist(HierarchyTrips$labels),Weight=rowSums(S))
 
 write_csv(weights,'Results/weights.csv')
+
+
+#Constructs weights using regional expenditure
+
+RegExpend <- read_csv("StateTourismExp.csv",skip = 5,col_names = TRUE)[,-(1:3)]
+
+TotRegExp <- RegExpend %>% colSums()
+TotTrips <- OvernightTrips_Region %>% colSums() 
+
+btm_weights <- TotRegExp/TotTrips
+
+AllWeights <- S %*% btm_weights
+
